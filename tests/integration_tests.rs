d861a5468eb1a574d2e6a46cec9a4eb657e5f516
Jan Frase <mrunkioiw@gmail.com>
2025-10-07 21:28:23 +0200

    Fix: Typo in black long castle.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -4,26 +4,30 @@ use perft_fixtures::perft_fixtures::{FAST_PERFT, NORMAL_PERFT, PerftFixture};
 
 #[test]
 fn test_perft_fast() {
-    test_perft(&FAST_PERFT);
+    test_perft_fixtures(&FAST_PERFT);
 }
 
 #[test]
 fn test_perft_normal() {
-    test_perft(&NORMAL_PERFT);
+    test_perft_fixtures(&NORMAL_PERFT);
 }
 
-fn test_perft(perft_fixtures: &[PerftFixture]) {
+fn test_perft_fixtures(perft_fixtures: &[PerftFixture]) {
     for perft_fixture in perft_fixtures {
-        let fen = perft_fixture.perft_setup.fen;
-        let depth = perft_fixture.depth;
-        let expected_nodes = perft_fixture.expected_nodes;
-        let name = perft_fixture.perft_setup.name.to_owned();
-
-        let mut state = GameState::new_from_fen(fen);
-        let nodes = perft(&mut state, depth);
-
-        assert_eq!(nodes, expected_nodes, "Testing {:?}", name);
+        test_single_perft_fixture(perft_fixture);
     }
+}
+
+fn test_single_perft_fixture(perft_fixture: &PerftFixture) {
+    let fen = perft_fixture.perft_setup.fen;
+    let depth = perft_fixture.depth;
+    let expected_nodes = perft_fixture.expected_nodes;
+    let name = perft_fixture.perft_setup.name.to_owned();
+
+    let mut state = GameState::new_from_fen(fen);
+    let nodes = perft(&mut state, depth);
+
+    assert_eq!(nodes, expected_nodes, "Testing {:?}", name);
 }
 
 // --------------------------------------------- //
