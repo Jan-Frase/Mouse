d7645433dc2a31c1280cc49d930bb85d7d5dc6be
jan <jan.frase@ovgu.de>
2025-10-11 20:56:28 +0200

    Feat: Increased compile settings.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ use std::env::Args;
 mod backend;
 
 fn main() {
+    let state =
+        State::new_from_fen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
+    let _ = perft(&state, 5);
     // let args = env::args();
     // run_perftree_debug(args);
 }
