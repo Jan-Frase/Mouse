0e46c9f99ffacb194f60c5701db20f0f3564f642
jan <jan.frase@ovgu.de>
2025-10-12 09:19:08 +0200

    Fix: Enabled perftree again.

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,14 @@ use crate::backend::movegen::moove::Moove;
 use crate::backend::movegen::move_gen::get_pseudo_legal_moves;
 use crate::backend::perft::perft;
 use crate::backend::state::game::state::State;
+use std::env;
 use std::env::Args;
 
 mod backend;
 
 fn main() {
-    let state =
-        State::new_from_fen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
-    let _ = perft(&state, 5);
-    // let args = env::args();
-    // run_perftree_debug(args);
+    let args = env::args();
+    run_perftree_debug(args);
 }
 
 // --------------------------------------------- //
@@ -36,31 +34,31 @@ pub fn run_perftree_debug(mut input: Args) {
             .split_whitespace()
             .map(Moove::new_from_uci_notation)
             .for_each(|moove| {
-                game_state.make_move(moove);
+                game_state = game_state.make_move(moove);
             });
     }
 
     root_debug_perft(&mut game_state, depth as u8);
 }
 
-pub fn root_debug_perft(game_state: &mut State, depth: u8) -> u64 {
+pub fn root_debug_perft(root_state: &mut State, depth: u8) -> u64 {
     // Total nodes searched.
     let mut nodes = 0;
 
     // Generate all root moves.
-    let mut moves = get_pseudo_legal_moves(game_state);
+    let mut moves = get_pseudo_legal_moves(root_state);
     // Sort them in the same way as perftree does
     moves.sort();
     for chess_move in moves {
-        game_state.make_move(chess_move);
+        let state = root_state.make_move(chess_move);
         // If we are in check after making the move -> skip.
-        if is_in_check(game_state, game_state.active_color().opposite()) {
+        if is_in_check(&state, state.active_color().opposite()) {
             // game_state.unmake_move(chess_move);
             continue;
         }
 
         // Recursively calculate nodes for this position.
-        let nodes_for_this_position = perft(game_state, depth - 1);
+        let nodes_for_this_position = perft(&state, depth - 1);
         nodes += nodes_for_this_position;
         // print info for https://github.com/agausmann/perftree
         println!("{} {:?}", chess_move, nodes_for_this_position);
